0602b41285c471b96c17eb9c114600b3ccef8e69
BASNUGROHO\basnugroho <s6134117@student.ubaya.ac.id>
2018-01-21 12:07:14 +0700

    Display the list of customers and movies

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 
@@ -9,24 +7,19 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        // GET: Customers
         public ViewResult Index()
         {
             var customers = GetCustomers();
 
-            return View();
+            return View(customers);
         }
 
         public ActionResult Details(int id)
-        { 
+        {
             var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
-            //var customer = (from cust in GetCustomers() where cust.Id == id select cust).SingleOrDefault();
-            //SingleOrDefault => "Returns the only element of a sequence, or a default value if the sequence is empty; "
 
             if (customer == null)
-            {
                 return HttpNotFound();
-            }
 
             return View(customer);
         }
@@ -35,11 +28,9 @@ namespace Vidly.Controllers
         {
             return new List<Customer>
             {
-                new Customer {Id = 1, Name = "Baskoro Nugroho"},
-                new Customer {Id = 2, Name = "Son Goku"}
+                new Customer { Id = 1, Name = "Baskoro Nugroho" },
+                new Customer { Id = 2, Name = "Son Goku" }
             };
         }
-
     }
-
 }
