4b0c404f91f5d33711cddfc8efbca5b4342288d0
BASNUGROHO\basnugroho <s6134117@student.ubaya.ac.id>
2018-01-21 11:31:13 +0700

    Use View Model + extract NavBar to Partial View

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -12,8 +12,22 @@ namespace Vidly.Controllers
         // GET: Movies
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            var movie = new Movie { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
+
+
     }
 }
