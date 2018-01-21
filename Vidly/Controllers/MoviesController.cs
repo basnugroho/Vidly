8f9749ec899cede221a0c2ee85d3985a9e4d7e9e
BASNUGROHO\basnugroho <s6134117@student.ubaya.ac.id>
2018-01-21 10:59:58 +0700

    MVC start

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            retrun view (movie);
+        }
+    }
+}
